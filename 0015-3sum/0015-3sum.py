048897f62f0cf680eeacba073e922ca617cb00ec
Anurag Tilwe <75813383+tilwe28@users.noreply.github.com>
2023-07-21 08:37:22 -0400

    Time: 1023 ms (93.49%), Space: 20.4 MB (89.50%) - LeetHub

--- 0015-3sum/0015-3sum.py
+++ 0015-3sum/0015-3sum.py
@@ -7,7 +7,7 @@ class Solution:
                 break
             if i > 0 and n == nums[i-1]:
                 continue
-
+            
             l, r = i + 1, len(nums) - 1
             while l < r:
                 currSum = n + nums[l] + nums[r]
@@ -18,8 +18,6 @@ class Solution:
                 else:
                     triplets.append([n, nums[l], nums[r]])
                     l += 1
-                    r -= 1
-                    while l < r and nums[l] == nums[l - 1]:
+                    while l < r and nums[l] == nums[l-1]:
                         l += 1
-
         return triplets
