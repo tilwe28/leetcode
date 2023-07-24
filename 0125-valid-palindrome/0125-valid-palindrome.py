3723ff3bf1168030e5469c7a2cfb7933fbb9a449
Anurag Tilwe <75813383+tilwe28@users.noreply.github.com>
2023-07-24 11:01:18 -0400

    Time: 93 ms (9.43%), Space: 17 MB (71.52%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -1,32 +1,36 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-        """
-        condensed = ""
-        for c in s:
-            if c.isalnum():
-                condensed += c.lower()
-        for i in range(len(condensed)):
-            if condensed[i] != condensed[(i+1)*-1]:
-                return False
-        return True
-
-        O(n) time complexity
-        O(n) space complexity
-        """
-
-        # Better solution (technically)
-        def alnum(c: str):
+        def isAlnum(c: str):
             return (
-                ord('A') <= ord(c) <= ord('Z')
-                or ord('a') <= ord(c) <= ord('z')
-                or ord('0') <= ord(c) <= ord('9')
+                ord('a') <= ord(c) <= ord('z') or
+                ord('A') <= ord(c) <= ord('Z') or
+                ord('0') <= ord(c) <= ord('9')
             )
 
+        """
+        # method 1
+        condensed = ""
+        for c in s:
+            if isAlnum(c):
+                condensed += c.lower()
+        l, r = 0, len(condensed) - 1
+        while l < r:
+            if condensed[l] != condensed[r]:
+                return False
+            l += 1
+            r -= 1
+        return True
+
+        # O(n) time
+        # O(n) space
+        """
+
+        # method 2
         l, r = 0, len(s) - 1
         while l < r:
-            while l < r and not alnum(s[l]):
+            while l < r and not isAlnum(s[l]):
                 l += 1
-            while l < r and not alnum(s[r]):
+            while l < r and not isAlnum(s[r]):
                 r -= 1
             if s[l].lower() != s[r].lower():
                 return False
@@ -34,5 +38,5 @@ class Solution:
             r -= 1
         return True
 
-        # O(n) time complexity
-        # O(1) space complexity
+        # O(n) time
+        # O(1) space
