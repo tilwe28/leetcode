7fc6ddba600f0ca53a015223343e31175bc7b2bc
Anurag Tilwe <75813383+tilwe28@users.noreply.github.com>
2025-02-24 23:15:46 +0100

    Time: 15 ms (99.76%), Space: 34.9 MB (89.45%) - LeetHub

--- 0245-shortest-word-distance-iii/0245-shortest-word-distance-iii.py
+++ 0245-shortest-word-distance-iii/0245-shortest-word-distance-iii.py
@@ -1,54 +1,16 @@
 class Solution:
     def shortestWordDistance(self, wordsDict: List[str], word1: str, word2: str) -> int:
-        w1_idx = [i for i, w in enumerate(wordsDict) if w == word1]
-        w2_idx = [i for i, w in enumerate(wordsDict) if w == word2]
-        w1_idx.sort()
-        w2_idx.sort()
-        print(w1_idx)
-        print(w2_idx)
-    
         min_dist = len(wordsDict)
-        p1, p2 = 0, 0
+        prev_idx = -1
+        is_same = word1 == word2
 
-        while p1 < len(w1_idx) and p2 < len(w2_idx):
-            i, j = w1_idx[p1], w2_idx[p2]
-            if i == j:
-                if p1 <= p2:
-                    p1 += 1
-                else:
-                    p2 += 1
-                continue
-
-            min_dist = min(min_dist, abs(i - j))
-            if i < j:
-                p1 += 1
-            else:
-                p2 += 1
-
-        while p1 < len(w1_idx):
-            i = w1_idx[p1]
-            j = w2_idx[-1]
-
-            if i == j:
-                p1 += 1
-                continue
-
-            min_dist = min(min_dist, abs(i - j))
-            p1 += 1
-
-        while p2 < len(w2_idx):
-            i = w1_idx[-1]
-            j = w2_idx[p2]
-
-            if i == j:
-                p2 += 1
-                continue
-
-            min_dist = min(min_dist, abs(i - j))
-            p2 += 1
+        for i, word in enumerate(wordsDict):
+            if word == word1 or word == word2:
+                if prev_idx != -1 and (is_same or word != wordsDict[prev_idx]):
+                    min_dist = min(min_dist, i - prev_idx)
+                prev_idx = i
 
         return min_dist
-
 
 """
 BRUTE FORCE:
@@ -65,4 +27,12 @@ INITIAL THOUGHTS:
     - use 2 pointer approach to find min
 Time: O(n log n)
 Space: O(n)
+
+Optimal:
+- iterate through dict
+- track index of prev word (word1 or word2)
+    - flag for if word1 == word2
+- track min_dist
+Time: O(n)
+Space: O(1)
 """
