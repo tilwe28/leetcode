ec1d31d8582040d9e65d9a0f624f94874dca5975
Anurag Tilwe <anurag.tilwe6@gmail.com>
2025-09-30 15:41:05 -0400

    Time: 0 ms (100%), Space: 18.2 MB (22.6%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
@@ -1,59 +1,22 @@
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
-        start, end = 0, len(nums) - 1
-        while start < end:
-            mid = (start + end) // 2
-            if target == nums[mid]:
+        l, r = 0, len(nums) - 1
+        while l <= r:
+            mid = (l + r) // 2
+            if nums[mid] == target:
                 return mid
-            if target == nums[end]:
-                return end
-
-            # case 1 (sorted)
-            if nums[mid] > nums[start] and nums[mid] < nums[end]:
+            elif nums[mid] <= nums[r]: # right is sorted
                 if target < nums[mid]:
-                    end = mid - 1
+                    r = mid - 1
+                elif target > nums[r]:
+                    r = mid - 1
                 else:
-                    start = mid + 1
-
-            # case 2 (min is on left)
-            elif nums[mid] < nums[start] and nums[mid] < nums[end]:
-                if target > nums[mid] and target < nums[start]:
-                    start = mid + 1
+                    l = mid + 1
+            else: # left is sorted
+                if target > nums[mid]:
+                    l = mid + 1
+                elif target < nums[l]:
+                    l = mid + 1
                 else:
-                    end = mid - 1
-
-            # case 3 (min is on right)
-            elif nums[mid] > nums[start] and nums[mid] > nums[end]:
-                if target < nums[mid] and target >= nums[start]:
-                    end = mid - 1
-                else:
-                    start = mid + 1
-            
-            else:
-                break
-
-        return -1 if target != nums[start] else start
-
-"""
-BINARY SEARCH
-~ use pointers for start and end
-~ loop while start < end
-
-checking type of rotation
-case 1 (sorted)
-    - is sorted if mid is > start and < end
-case 2 (min is on left side)
-    - occurs if mid < start and < end
-case 3 (min is on right side)
-    - occurs if mid > start and > end
-
-checking target location based on rotation type
-case 1
-    - regular binary search
-case 2
-    - if target > mid and < start, then target is on right side, else target on left
-case 3
-    - if target < mid and > start, then target is on left side, else target on right
-
-check if mid == target
-"""
+                    r = mid - 1
+        return -1
