d0f4df38def20ed8ad29aa836d863b86400daaa5
Anurag Tilwe <anurag.tilwe6@gmail.com>
2025-09-30 01:23:59 -0400

    Time: 19 ms (36.99%), Space: 22.4 MB (36.1%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -1,10 +1,24 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        groups = collections.defaultdict(list)
-        
+        anagrams = {}
         for s in strs:
             letters = [0] * 26
             for c in s:
                 letters[ord(c) - ord('a')] += 1
-            groups[tuple(letters)].append(s)
-        return groups.values()
+            letters = tuple(letters)
+            if anagrams.get(letters):
+                anagrams[letters].append(s)
+            else:
+                anagrams[letters] = [s]
+        return list(anagrams.values())
+
+"""
+Initial Thoughts:
+- loop through list of strs
+- have some type of mapping for anagram representation and list of strs that satisfy it
+- for each str in strs, check if str's anagram representation exists in mapping
+    - if so, add str to list
+    - else, create new mapping with str in the list
+
+- key for mapping will be tuple of size 26 where the ith index is frequency of that character
+"""
