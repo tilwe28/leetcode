9c7842783de351bcb55bc69291221c503c09ba2e
Anurag Tilwe <anurag.tilwe6@gmail.com>
2025-09-30 12:57:19 -0400

    Time: 423 ms (93.77%), Space: 20.9 MB (19.32%) - LeetHub

--- 0015-3sum/0015-3sum.py
+++ 0015-3sum/0015-3sum.py
@@ -1,23 +1,24 @@
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
         nums.sort()
-        triplets = []
-        for i,n in enumerate(nums):
+        res = []
+        for i, n in enumerate(nums):
             if n > 0:
-                break
-            if i > 0 and n == nums[i-1]:
-                continue
-            
+                break # since list is sorted, there won't be any more negatives (needed for sum to be 0)
+            if i > 0 and nums[i] == nums[i - 1]:
+                continue # already considered number and cannot have duplicates
+
             l, r = i + 1, len(nums) - 1
             while l < r:
-                currSum = n + nums[l] + nums[r]
-                if currSum < 0:
+                curr_sum = n + nums[l] + nums[r]
+                if curr_sum == 0:
+                    res.append([n, nums[l], nums[r]])
                     l += 1
-                elif currSum > 0:
                     r -= 1
-                else:
-                    triplets.append([n, nums[l], nums[r]])
-                    l += 1
-                    while l < r and nums[l] == nums[l-1]:
+                    while l < r and nums[l] == nums[l - 1]:
                         l += 1
-        return triplets
+                elif curr_sum < 0:
+                    l += 1
+                else:
+                    r -= 1
+        return res
