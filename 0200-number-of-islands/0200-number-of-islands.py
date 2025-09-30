55f2f5f56d266d08323828c59a3a3bca46d6e637
Anurag Tilwe <anurag.tilwe6@gmail.com>
2025-09-30 19:44:17 -0400

    Time: 224 ms (96.03%), Space: 20.3 MB (43.05%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -1,32 +1,27 @@
-from collections import deque
-
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-        num_islands = 0
+        count = 0
+        for i, row in enumerate(grid):
+            for j, val in enumerate(row):
+                if val == '1':
+                    count += 1
+                    stack = []
+                    stack.append((i, j))
+                    while stack:
+                        r, c = stack.pop()
+                        grid[r][c] = 'x'
+                        if r > 0 and grid[r - 1][c] == '1':
+                            stack.append((r - 1, c))
+                        if r + 1 < len(grid) and grid[r + 1][c] == '1':
+                            stack.append((r + 1, c))
+                        if c > 0 and grid[r][c - 1] == '1':
+                            stack.append((r, c - 1))
+                        if c + 1 < len(row) and grid[r][c + 1] == '1':
+                            stack.append((r, c + 1))
+        return count
 
-        for r in range(len(grid)):
-            for c in range(len(grid[0])):
-                # start of island
-                if grid[r][c] == '1':
-                    num_islands += 1
-
-                    # BFS
-                    grid[r][c] = '-'
-                    q = deque()
-                    q.append((r, c))
-                    while q:
-                        rn, cn = q.popleft()
-                        if rn + 1 < len(grid) and grid[rn + 1][cn] == '1':
-                            q.append((rn + 1, cn))
-                            grid[rn + 1][cn] = '-'
-                        if cn + 1 < len(grid[0]) and grid[rn][cn + 1] == '1':
-                            q.append((rn, cn + 1))
-                            grid[rn][cn + 1] = '-'
-                        if rn - 1 >= 0 and grid[rn - 1][cn] == '1':
-                            q.append((rn - 1, cn))
-                            grid[rn - 1][cn] = '-'
-                        if cn - 1 >= 0 and grid[rn][cn - 1] == '1':
-                            q.append((rn, cn - 1))
-                            grid[rn][cn - 1] = '-'
-
-        return num_islands
+"""
+- iterate through grid
+- if a 1 is spotten, increment the count and run bfs
+- during dfs, mark all 1s in island as 'x'
+"""
