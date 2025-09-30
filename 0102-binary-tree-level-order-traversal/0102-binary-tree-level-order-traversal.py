7b6439adcf2ac40d0a2e0e335d6c13065144ff1c
Anurag Tilwe <anurag.tilwe6@gmail.com>
2025-09-30 17:55:56 -0400

    Time: 0 ms (100%), Space: 18.4 MB (59.99%) - LeetHub

--- 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.py
+++ 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.py
@@ -7,35 +7,19 @@
 class Solution:
     def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
         if not root:
-            return None
-        
-        res, q = [], []
+            return []
+        res = []
+        q = []
         q.append(root)
         while q:
             level = []
-            for i in range(len(q)):
-                node = q.pop(0)
-                level.append(node.val)
-                if node.left:
-                    q.append(node.left)
-                if node.right:
-                    q.append(node.right)
+            n = len(q)
+            for _ in range(n):
+                curr = q.pop(0)
+                if curr.left:
+                    q.append(curr.left)
+                if curr.right:
+                    q.append(curr.right)
+                level.append(curr.val)
             res.append(level)
         return res
-
-"""
-BINARY TREE
-- level order traversal means bfs
-- to do bfs use queue to store nodes to visit
-- if nodes have child continue to add to queue
-- keep going while there are nodes in queue
-- each level is defined by nodes in queue at each iteration
-
-- have result list
-- have outer while loop that continues as long as there are nodes in queue
-- create level list
-- have inner for loop that iterates len(queue) amount of times
-    - dequeue node and add to level list
-    - add nodes children to queue
-- add level list to result
-"""
