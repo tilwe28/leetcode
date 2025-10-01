73509d7d35c3e0ef16dd78670a30e87e916603ad
Anurag Tilwe <anurag.tilwe6@gmail.com>
2025-10-01 17:36:41 -0400

    Time: 1536 ms (33.8%), Space: 42.8 MB (38.5%) - LeetHub

--- 0718-maximum-length-of-repeated-subarray/0718-maximum-length-of-repeated-subarray.py
+++ 0718-maximum-length-of-repeated-subarray/0718-maximum-length-of-repeated-subarray.py
@@ -1,21 +1,28 @@
 class Solution:
     def findLength(self, nums1: List[int], nums2: List[int]) -> int:
-        max_len = 0
         dp = [[0] * (len(nums2) + 1) for _ in range(len(nums1) + 1)]
+        res = 0
         for i in range(1, len(nums1) + 1):
             for j in range(1, len(nums2) + 1):
                 if nums1[i - 1] == nums2[j - 1]:
-                    dp[i][j] = dp[i-1][j-1] + 1
-                    max_len = max(max_len, dp[i][j])
-        return max_len
+                    dp[i][j] = dp[i - 1][j - 1] + 1
+                    res = max(res, dp[i][j])
+                else:
+                    dp[i][j] = 0
+        return res
 
 """
-BRUTE FORCE:
-- check all subarrays
-Time: O(n^2)
-Space: O(n^2)
+  3 2 1 4 7
+1 0 0 1 0 0
+2 0 1 0 0 0
+3 1 0 0 0 0
+2 0 2 0 0 0
+1 0 0 3 0 0
 
-DP:
-- subproblem is if you keep taking off last element from nums
-- base case is when length is 0
+  1 0 1 0 1
+0 0 1 0 0 0
+1 1 0 2 0 1
+1 1 0 1 0 1
+1 1 0 1 0 1
+1 1 0 1 0 1
 """
