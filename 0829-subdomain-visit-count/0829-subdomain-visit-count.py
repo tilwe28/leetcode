b95ac322119797653d0520c30dc574fa86f686bb
Anurag Tilwe <anurag.tilwe6@gmail.com>
2025-10-01 17:05:13 -0400

    Time: 3 ms (92.24%), Space: 17.9 MB (52.08%) - LeetHub

--- 0829-subdomain-visit-count/0829-subdomain-visit-count.py
+++ 0829-subdomain-visit-count/0829-subdomain-visit-count.py
@@ -1,26 +1,17 @@
 class Solution:
     def subdomainVisits(self, cpdomains: List[str]) -> List[str]:
         domain_count = {}
-        for cpdomain in cpdomains:
-            count, url = cpdomain.split(" ")
+        for cp_domain in cpdomains:
+            count, domain = cp_domain.split(' ')
             count = int(count)
 
-            domains = url.split(".")
-            for i in range(len(domains), 0, -1):
-                domain = '.'.join(domains[i-1:])
-                domain_count[domain] = domain_count.get(domain, 0) + count
-
+            domains = domain.split('.')
+            for i in range(len(domains)):
+                subdomain = '.'.join(domains[i:])
+                domain_count[subdomain] = domain_count.get(subdomain, 0) + count
+        
         res = []
         for domain, count in domain_count.items():
             res.append(f"{count} {domain}")
-
+        
         return res
-
-"""
-INITIAL THOUGHTS:
-- map of domains to count
-- for each url, split into different domains
-    - upadte domain count
-Time: O(n)
-Space: O(n)
-"""
