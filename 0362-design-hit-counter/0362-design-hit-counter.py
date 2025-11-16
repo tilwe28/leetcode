e0b22ab847a4492d1350f321fda4b39aa50e551f
Anurag Tilwe <anurag.tilwe6@gmail.com>
2025-11-16 02:18:26 -0500

    Time: 0 ms (100%), Space: 18.1 MB (19.41%) - LeetHub

--- 0362-design-hit-counter/0362-design-hit-counter.py
+++ 0362-design-hit-counter/0362-design-hit-counter.py
@@ -2,13 +2,17 @@ class HitCounter:
 
     def __init__(self):
         self.hits = defaultdict(int)
+        self.min = sys.maxsize
+        self.max = -sys.maxsize
 
     def hit(self, timestamp: int) -> None:
         self.hits[timestamp] += 1
+        self.min = min(self.min, timestamp)
+        self.max = max(self.max, timestamp)
 
     def getHits(self, timestamp: int) -> int:
         res = 0
-        for time in range(timestamp - 300 + 1, timestamp + 1):
+        for time in range(max(self.min, timestamp - 300 + 1), min(self.max, timestamp) + 1):
             if time not in self.hits:
                 continue
             res += self.hits[time]
