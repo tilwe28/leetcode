bcf6a50f62888c9794dd89d51cade8c8c5022260
Anurag Tilwe <anurag.tilwe6@gmail.com>
2025-11-18 22:07:37 -0500

    Time: 0 ms (100%), Space: 17.8 MB (54.07%) - LeetHub

--- 0394-decode-string/0394-decode-string.py
+++ 0394-decode-string/0394-decode-string.py
@@ -1,46 +1,43 @@
 class Solution:
-    def decodeSubString(self, s: str) -> (str, int):
-        res = ""
-        i = 0
-        while i < len(s):
-            if s[i].isalpha():
-                res += s[i]
-            elif s[i].isdigit():
-                j = s[i:].find('[')
-                k = int(s[i:i+j])
-                substr, i_inc = self.decodeSubString(s[i+j+1:])
-                res += substr * k
-                i = i + j + i_inc + 1
-            elif s[i] == ']':
-                return res, i
-            i += 1
-
     def decodeString(self, s: str) -> str:
-        res = ""
         i = 0
-        while i < len(s):
-            if s[i].isalpha():
-                res += s[i]
-            elif s[i].isdigit():
-                j = s[i:].find('[')
-                k = int(s[i:i+j])
-                substr, i_inc = self.decodeSubString(s[i+j+1:])
-                res += substr * k
-                i = i + j + i_inc + 1
-            i += 1
-        
-        return res
+        def decode() -> str:
+            nonlocal i
+            res = []
+            k = []
+            while i < len(s):
+                if s[i].isdigit():
+                    k.append(s[i])
+                    i += 1
+                elif s[i] == '[':
+                    i += 1
+                    res.append(decode() * int(''.join(k)))
+                    k = []
+                elif s[i] == ']':
+                    i += 1
+                    return ''.join(res)
+                else:
+                    res.append(s[i])
+                    i += 1
             
+            return ''.join(res)
+
+        return decode()
 
 """
-result string
-for normal characters, just add it to the result
-if a number digit is seen, then get the number by taking the substring from the first digit until the opening bracket
-- i = idx of digit, j = s[i:].find('['), k = int(s[i:j])
-- e = s[j:].find(']'), encoded_substr = s[j+1:e]
-- res += encoded_substr * k
-
-problem with above, it doesn't account for nested encodings
-- use a helper function that is called when an encoding is seen
-- return from the encoding when a ']' is seen
+initial thoughts:
+- iterate through the string
+- different cases of the character
+    - digit
+    - letter
+    - opening bracket
+    - closing bracket
+- if digit
+    - continue reading the number until '['
+- if opening bracket '['
+    - call helper function, append output * k
+- if closing bracket ']'
+    - return output
+- if letter
+    - keep appending to output
 """
